b72957c9b8af7ebbcaff6c595d9e311241306a4b
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-02-20 07:54:44 +0100

    search api built

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -25,10 +25,28 @@ export const ProductsList = () => {
       getProducts();
     }
   };
+  const handleSearch = async (event) => {
+    const key = event.target.value; //gets data written in input field
+    if (key) {
+      let result = await fetch(`http://localhost:5000/search/${key}`);
+      result = await result.json();
+      if (result) {
+        setProducts(result); // if result is true update initial state and display
+      }
+    } else {
+      getProducts();
+    }
+  };
 
   return (
     <div className="product-list">
       <h1>Product List</h1>
+      <input
+        className="search-box"
+        type=""
+        placeholder="search..."
+        onChange={handleSearch}
+      />
       <ul>
         <li>S. No.</li>
         <li> Name</li>
@@ -37,21 +55,25 @@ export const ProductsList = () => {
         <li>Category</li>
         <li>Action</li>
       </ul>
-      {products.map((prevProducts, index) => (
-        <ul key={prevProducts._id}>
-          <li>{index + 1}</li>
-          <li>{prevProducts.name}</li>
-          <li>{prevProducts.price}</li>
-          <li>{prevProducts.company}</li>
-          <li>{prevProducts.category}</li>
-          <li>
-            <button onClick={() => deleteProduct(prevProducts._id)}>
-              Delete
-            </button>
-            <Link to={`/update/${prevProducts._id}`}>Update</Link>
-          </li>
-        </ul>
-      ))}
+      {products.length > 0 ? (
+        products.map((prevProducts, index) => (
+          <ul key={prevProducts._id}>
+            <li>{index + 1}</li>
+            <li>{prevProducts.name}</li>
+            <li>{prevProducts.price}</li>
+            <li>{prevProducts.company}</li>
+            <li>{prevProducts.category}</li>
+            <li>
+              <button onClick={() => deleteProduct(prevProducts._id)}>
+                Delete
+              </button>
+              <Link to={`/update/${prevProducts._id}`}>Update</Link>
+            </li>
+          </ul>
+        ))
+      ) : (
+        <h1>No result...</h1>
+      )}
     </div>
   );
 };
